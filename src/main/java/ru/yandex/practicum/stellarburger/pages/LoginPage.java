c3bd1845e3cf358cb61b0e979f67eb1eb6086b96
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2022-07-26 11:42:01 +0300

    LoginPage

---
+++ src/main/java/ru/yandex/practicum/stellarburger/pages/LoginPage.java
@@ -0,0 +1,34 @@
+package ru.yandex.practicum.stellarburger.pages;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class LoginPage {
+    @FindBy(how = How.XPATH, using = ".//form[contains(@class,'Auth_form')]")
+    private SelenideElement loginForm;
+
+    @FindBy(how = How.XPATH, using = ".//label[text()='Email']/following-sibling::input")
+    private SelenideElement emailField;
+
+    @FindBy(how = How.XPATH, using = ".//label[text()='Пароль']/following-sibling::input")
+    private SelenideElement passwordField;
+
+    @FindBy(how = How.XPATH, using = ".//button[text()='Войти']")
+    private SelenideElement loginButton;
+
+    @FindBy(how = How.LINK_TEXT,using = "Зарегистрироваться")
+    private SelenideElement registerLink;
+
+    @Step("Click register link")
+    public void clickRegisterLink() {
+        registerLink.click();
+    }
+
+    @Step("Check login form is displayed")
+    public void checkLoginFormDisplayed() {
+        loginForm.shouldBe(Condition.visible);
+    }
+}
