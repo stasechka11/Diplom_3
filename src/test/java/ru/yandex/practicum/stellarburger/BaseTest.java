c6fe177bb611410bdfb9e094f3a95e1da46494e7
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2022-07-29 15:35:26 +0300

    committed using of yandex browser

--- src/test/java/ru/yandex/practicum/stellarburger/BaseTest.java
+++ src/test/java/ru/yandex/practicum/stellarburger/BaseTest.java
@@ -5,14 +5,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseTest {
-     ChromeDriver driver;
+    ChromeDriver driver;
 
-    public  void setUpBrowser() {
-        ChromeOptions options = new ChromeOptions();
+    public void setUpBrowser() {
+        /*ChromeOptions options = new ChromeOptions();
         System.setProperty("webdriver.chrome.driver", "WebDriver\\bin\\yandexdriver.exe");
         options.setBinary("C:\\Users\\Lenovo\\AppData\\Local\\Yandex\\YandexBrowser\\Application\\browser.exe");
+        options.addArguments("--start-maximized", "--disable-infobars");
         driver = new ChromeDriver(options);
-        WebDriverRunner.setWebDriver(driver);
+        WebDriverRunner.setWebDriver(driver);*/
     }
 
 }
